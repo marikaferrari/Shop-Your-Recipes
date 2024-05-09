cccf131ed459004af8e6fc873de616430293743f
Marika <ferrari.marika@gmail.com>
2024-05-09 14:01:21 +0100

    Combining every form of data binding

--- src/app/component-name/component-name.component.ts
+++ src/app/component-name/component-name.component.ts
@@ -24,7 +24,7 @@ export class ComponentNameComponent {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created';
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName ;
   }
 
   onUpdateServerName(event: any) {
