2f69ef56d2b9320db8640d592546e0deb43abbd5
Marika <ferrari.marika@gmail.com>
2024-05-10 14:33:58 +0100

    New project plan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  // *ngFor assignment
-  showSecret = false;
-  log = [];
-
-  // Each time the button is clicked
-  // This method is triggered and displays how many times it's been clicked
-  onToggleDetails() {
-    this.showSecret = !this.showSecret;
-    this.log.push(this.log.length + 1)
-  }
 
 }
