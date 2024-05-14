3fee51c9448a40e5e85951076b5be88b5ff5b8f2
Marika <ferrari.marika@gmail.com>
2024-05-14 16:49:00 +0100

    Added styling to recipe components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,8 +10,8 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://www.recipetineats.com/wp-content/uploads/2024/05/Pizza-Capricciosa_8.jpg?w=747&h=747&crop=1')
   ];
 
   constructor() { }
