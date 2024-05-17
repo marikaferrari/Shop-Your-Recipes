979d46ab2ebfc67a3718ab98f47dc61dc7a3e394
Marika <ferrari.marika@gmail.com>
2024-05-17 11:37:01 +0100

    Services for recipe and shopping list created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  loadedFeature = 'recipe';
 
+export class AppComponent {
+  loadedFeature = 'recipe'; // A property to keep track of the currently selected feature (default is 'recipe')
+
+  // Method to change the value of loadedFeature based on the user's navigation choice
   onNavigate(feature: string) {
     this.loadedFeature = feature;
   }
