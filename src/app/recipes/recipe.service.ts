4fbdfd78aed06803ed511b03b0a5700efeebddaa
Marika <ferrari.marika@gmail.com>
2024-05-17 15:15:26 +0100

    Added style to recipe-detail component

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,14 +2,24 @@ import { EventEmitter } from "@angular/core";
 
 // Model
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://www.recipetineats.com/wp-content/uploads/2024/05/Pizza-Capricciosa_8.jpg?w=747&h=747&crop=1')
+        new Recipe('Gazpacho', 'Delicious Andalusian gazpacho (with a "z").', 'https://images.unsplash.com/photo-1662469838214-a97415cd83fe?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        [
+        new Ingredient('Tomato', 2),
+        new Ingredient('Bread', 1),
+        new Ingredient('Red Pepper', 1)
+        ]),
+        new Recipe('Ravioli', 'Ravioli with hearbs and tomatoes and some more stuff.', 'https://images.unsplash.com/photo-1461009463816-4edea93afd6f?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', [
+            new Ingredient('Tomato', 1),
+            new Ingredient('Egg', 2),
+            new Ingredient('Olive oil', 1),
+        ])
       ];
 
     getRecipes() {
