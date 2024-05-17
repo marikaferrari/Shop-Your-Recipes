0b7174d30dd6c58b6999982ca8cfae35563241e1
Marika <ferrari.marika@gmail.com>
2024-05-17 12:22:57 +0100

    addIngredient method implemented

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -16,5 +16,6 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
+    this.slService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => this.ingredients = ingredients);
   }
 }
