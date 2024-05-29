dd124406b2a62253ae80adef66a3196a3f391fc3
Marika <ferrari.marika@gmail.com>
2024-05-29 16:56:49 +0100

    Refactored recipe-edit component with reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 // Needed for databinding
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // Components
 import { AppComponent } from './app.component';
@@ -41,6 +41,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [RecipeService, ShoppingListService],
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,91 +1,102 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
 
-// Model
-import { Recipe } from "./recipe.model";
-import { Ingredient } from "../shared/ingredient.model";
-
-// Service
-import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe('Gazpacho', 'Delicious Andalusian gazpacho (with a "z").', 'https://images.unsplash.com/photo-1662469838214-a97415cd83fe?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        [
-        new Ingredient('Tomato', 2),
-        new Ingredient('Bread', 1),
-        new Ingredient('Red Pepper', 1)
-        ]),
-        new Recipe('Herbs Ravioli', 'Ravioli with herbs and tomatoes and some more stuff.', 'https://images.unsplash.com/photo-1461009463816-4edea93afd6f?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', [
-            new Ingredient('Tomato', 1),
-            new Ingredient('Egg', 2),
-            new Ingredient('Olive oil', 1),
-        ]),
-        new Recipe('Cheese Sandwich', 'Bread, cheese, ham ... what can go wrong?', 'https://images.unsplash.com/photo-1528736235302-52922df5c122?q=80&w=2754&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        [
-        new Ingredient('Cheese', 2),
-        new Ingredient('Ham', 1),
-        new Ingredient('Bread', 1)
-        ]),
-        new Recipe('Seafood Pasta', 'Can you hear the call of the sea?', 'https://images.unsplash.com/photo-1563379926898-05f4575a45d8?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        [
-        new Ingredient('Pasta', 1),
-        new Ingredient('Shrimp', 10),
-        new Ingredient('Tomato', 5),
-        new Ingredient('Olive Oil', 1),
-        ]),
-        new Recipe('Omelette Du Fromage', 'Quite the omelette, Dexter guaranteed.', 'https://pbs.twimg.com/media/ChI8G3WWwAQXQby.jpg',
-        [
-        new Ingredient('Tomato', 2),
-        new Ingredient('Bread', 1),
-        new Ingredient('Red Pepper', 1)
-        ]),
-        new Recipe('Gazpacho', 'Delicious Andalusian gazpacho (with a "z").', 'https://images.unsplash.com/photo-1662469838214-a97415cd83fe?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        [
-        new Ingredient('Tomato', 2),
-        new Ingredient('Bread', 1),
-        new Ingredient('Red Pepper', 1)
-        ]),
-        new Recipe('Herbs Ravioli', 'Ravioli with herbs and tomatoes and some more stuff.', 'https://images.unsplash.com/photo-1461009463816-4edea93afd6f?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', [
-            new Ingredient('Tomato', 1),
-            new Ingredient('Egg', 2),
-            new Ingredient('Olive oil', 1),
-        ]),
-        new Recipe('Cheese Sandwich', 'Bread, cheese, ham ... what can go wrong?', 'https://images.unsplash.com/photo-1528736235302-52922df5c122?q=80&w=2754&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        [
-        new Ingredient('Cheese', 2),
-        new Ingredient('Ham', 1),
-        new Ingredient('Bread', 1)
-        ]),
-        new Recipe('Seafood Pasta', 'Can you hear the call of the sea?', 'https://images.unsplash.com/photo-1563379926898-05f4575a45d8?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        [
-        new Ingredient('Pasta', 1),
-        new Ingredient('Shrimp', 10),
-        new Ingredient('Tomato', 5),
-        new Ingredient('Olive Oil', 1),
-        ]),
-        new Recipe('Omelette Du Fromage', 'Quite the omelette, Dexter guaranteed.', 'https://pbs.twimg.com/media/ChI8G3WWwAQXQby.jpg',
-        [
+  private recipes: Recipe[] = [
+    new Recipe('Gazpacho', 'Delicious Andalusian gazpacho (with a "z").', 'https://images.unsplash.com/photo-1662469838214-a97415cd83fe?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    [
+    new Ingredient('Tomato', 2),
+    new Ingredient('Bread', 1),
+    new Ingredient('Red Pepper', 1)
+    ]),
+    new Recipe('Herbs Ravioli', 'Ravioli with herbs and tomatoes and some more stuff.', 'https://images.unsplash.com/photo-1461009463816-4edea93afd6f?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', [
+        new Ingredient('Tomato', 1),
         new Ingredient('Egg', 2),
-        new Ingredient('Fromage', 1),
-        ])
-      ];
+        new Ingredient('Olive oil', 1),
+    ]),
+    new Recipe('Cheese Sandwich', 'Bread, cheese, ham ... what can go wrong?', 'https://images.unsplash.com/photo-1528736235302-52922df5c122?q=80&w=2754&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    [
+    new Ingredient('Cheese', 2),
+    new Ingredient('Ham', 1),
+    new Ingredient('Bread', 1)
+    ]),
+    new Recipe('Seafood Pasta', 'Can you hear the call of the sea?', 'https://images.unsplash.com/photo-1563379926898-05f4575a45d8?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    [
+    new Ingredient('Pasta', 1),
+    new Ingredient('Shrimp', 10),
+    new Ingredient('Tomato', 5),
+    new Ingredient('Olive Oil', 1),
+    ]),
+    new Recipe('Omelette Du Fromage', 'Quite the omelette, Dexter guaranteed.', 'https://pbs.twimg.com/media/ChI8G3WWwAQXQby.jpg',
+    [
+    new Ingredient('Tomato', 2),
+    new Ingredient('Bread', 1),
+    new Ingredient('Red Pepper', 1)
+    ]),
+    new Recipe('Gazpacho', 'Delicious Andalusian gazpacho (with a "z").', 'https://images.unsplash.com/photo-1662469838214-a97415cd83fe?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    [
+    new Ingredient('Tomato', 2),
+    new Ingredient('Bread', 1),
+    new Ingredient('Red Pepper', 1)
+    ]),
+    new Recipe('Herbs Ravioli', 'Ravioli with herbs and tomatoes and some more stuff.', 'https://images.unsplash.com/photo-1461009463816-4edea93afd6f?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', [
+        new Ingredient('Tomato', 1),
+        new Ingredient('Egg', 2),
+        new Ingredient('Olive oil', 1),
+    ]),
+    new Recipe('Cheese Sandwich', 'Bread, cheese, ham ... what can go wrong?', 'https://images.unsplash.com/photo-1528736235302-52922df5c122?q=80&w=2754&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    [
+    new Ingredient('Cheese', 2),
+    new Ingredient('Ham', 1),
+    new Ingredient('Bread', 1)
+    ]),
+    new Recipe('Seafood Pasta', 'Can you hear the call of the sea?', 'https://images.unsplash.com/photo-1563379926898-05f4575a45d8?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    [
+    new Ingredient('Pasta', 1),
+    new Ingredient('Shrimp', 10),
+    new Ingredient('Tomato', 5),
+    new Ingredient('Olive Oil', 1),
+    ]),
+    new Recipe('Omelette Du Fromage', 'Quite the omelette, Dexter guaranteed.', 'https://pbs.twimg.com/media/ChI8G3WWwAQXQby.jpg',
+    [
+    new Ingredient('Egg', 2),
+    new Ingredient('Fromage', 1),
+    ])
+  ];
 
-    constructor(private slService: ShoppingListService) {
-        this.slService;
-    }
+  constructor(private slService: ShoppingListService) {}
 
-    getRecipes() {
-        // slice creates a new array, a copy of recipes
-        return this.recipes.slice();
-    }
+  getRecipes() {
+    return this.recipes.slice();
+  }
 
-    getRecipe(index: number) {
-        return this.recipes[index];
-    }
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
 
-    addIngredientsToShoppingList(ingredients: Ingredient[]) {
-        this.slService.addIngredientsFromRecipe(ingredients);
-    }
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
 }
