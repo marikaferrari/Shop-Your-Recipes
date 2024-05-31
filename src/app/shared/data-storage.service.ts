af1d85571dc89fc0679e21c4a47daf1b54e05d4c
Marika <ferrari.marika@gmail.com>
2024-05-31 16:38:41 +0100

    Transofrming response data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -18,9 +19,21 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http.get<Recipe[]>(this.url).subscribe(recipes => {
-      // console.log(recipes);
-      this.recipeService.setRecipes(recipes);
-    });
+    this.http
+      .get<Recipe[]>(this.url)
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        })
+      )
+      .subscribe((recipes) => {
+        // console.log(recipes);
+        this.recipeService.setRecipes(recipes);
+      });
   }
 }
