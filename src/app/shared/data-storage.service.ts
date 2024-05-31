09d6a2d26621438e9bc4bd51cc4f08f951c85366
Marika <ferrari.marika@gmail.com>
2024-05-31 16:21:40 +0100

    Eslint file added

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class DataStorageService {
-    constructor(private http: HttpClient) {
-        
-    }
+  private url =
+    'https://shop-your-recipes-default-rtdb.europe-west1.firebasedatabase.app/recipes.json';
+
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put(this.url, recipes).subscribe(response => {
+        console.log(response);
+    });
+  }
 }
