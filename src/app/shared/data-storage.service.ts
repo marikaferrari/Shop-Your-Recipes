b69d9a4ea05c8b116351be85567c53ffa345ee74
Marika <ferrari.marika@gmail.com>
2024-05-31 16:53:57 +0100

    Resolver

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,39 +1,46 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
+
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
-import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  private url =
-    'https://shop-your-recipes-default-rtdb.europe-west1.firebasedatabase.app/recipes.json';
+
+  private url = 'https://shop-your-recipes-default-rtdb.europe-west1.firebasedatabase.app/recipes.json'
 
   constructor(private http: HttpClient, private recipeService: RecipeService) {}
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
-    this.http.put(this.url, recipes).subscribe((response) => {
-      // console.log(response);
-    });
+    this.http
+      .put(
+        this.url,
+        recipes
+      )
+      .subscribe(response => {
+        console.log(response);
+      });
   }
 
   fetchRecipes() {
-    this.http
-      .get<Recipe[]>(this.url)
+    return this.http
+      .get<Recipe[]>(
+        this.url
+      )
       .pipe(
-        map((recipes) => {
-          return recipes.map((recipe) => {
+        map(recipes => {
+          return recipes.map(recipe => {
             return {
               ...recipe,
-              ingredients: recipe.ingredients ? recipe.ingredients : [],
+              ingredients: recipe.ingredients ? recipe.ingredients : []
             };
           });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
         })
       )
-      .subscribe((recipes) => {
-        // console.log(recipes);
-        this.recipeService.setRecipes(recipes);
-      });
   }
 }
