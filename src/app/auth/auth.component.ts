30b4429b293ce23c9821fb585542ca75e4aa3bba
Marika <ferrari.marika@gmail.com>
2024-06-03 11:06:43 +0100

    Sending the signup request

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,20 +1,40 @@
-import { Component } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { AuthService } from './auth.service';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-    selector: 'app-auth',
-    templateUrl: './auth.component.html'
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
 })
-
 export class AuthComponent {
-    isLoginMode: boolean = true;
+  isLoginMode: boolean = true;
 
-    onSwitchMode() {
-        this.isLoginMode = !this.isLoginMode;
+  constructor(private authService: AuthService) {}
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    // console.log(form.value);
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if (this.isLoginMode) {
+    } else {
+      this.authService.signUp(email, password).subscribe(
+        (resData) => {
+          console.log(resData);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
     }
 
-    onSubmit(form: NgForm) {
-        console.log(form.value);
-        form.reset();
-    }
+    form.reset();
+  }
 }
