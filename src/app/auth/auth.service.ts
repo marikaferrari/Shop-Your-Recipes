68007bafbfeac27e0283073179884f5666247619
Marika <ferrari.marika@gmail.com>
2024-06-03 11:37:31 +0100

    Seinding login requests

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
@@ -26,7 +27,7 @@ export class AuthService {
         catchError((errorRes) => {
           let errorMessage = 'An unknown error occured!';
           if (!errorRes.error || !errorRes.error.error) {
-              return throwError(errorMessage)
+            return throwError(errorMessage);
           }
           switch (errorRes.error.error.message) {
             case 'EMAIL_EXISTS':
@@ -35,5 +36,12 @@ export class AuthService {
           return throwError(errorMessage);
         })
       );
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyALxQsJ1sa6MDkuPrsdSI732rGC-m5W7sY',
+      { email: email, password: password, returnSecureToken: true }
+    );
   }
 }
