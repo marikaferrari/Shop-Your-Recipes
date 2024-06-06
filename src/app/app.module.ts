0d91bf30348118267dbe3e2fa35d1ee5e9fa951b
Marika <ferrari.marika@gmail.com>
2024-06-06 17:30:11 +0100

    Reverted back to original app module due to vercel errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 // Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AlertComponent } from './shared/alert/alert.component';
@@ -25,16 +31,21 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
 
 // Module
 import { AppRoutingModule } from './app-routing.module';
-import { RecipesModule } from './recipes/recipes.modules';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
+    RecipeStartComponent,
+    RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
     AlertComponent,
@@ -46,7 +57,6 @@ import { RecipesModule } from './recipes/recipes.modules';
     ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule,
-    RecipesModule
   ],
   providers: [
     RecipeService,
