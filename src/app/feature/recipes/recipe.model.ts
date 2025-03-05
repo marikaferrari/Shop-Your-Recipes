788ec0dcf740f632dc1b6e1d42781b7314c61c57
Marika <ferrari.marika@gmail.com>
2025-03-05 10:50:08 +0000

    Ui recipe details fixes

--- src/app/feature/recipes/recipe.model.ts
+++ src/app/feature/recipes/recipe.model.ts
@@ -1,6 +1,7 @@
 import { Ingredient } from "../../core/models/ingredient.model";
 
 export class Recipe {
+  public id: number;
   public name: string;
   public description: string;
   public imagePath: string;
